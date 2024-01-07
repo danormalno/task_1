e776794c99265e6280ecdd1f1f7c4202ee3d45f0
danormalno <pvdieta@gmail.com>
2024-01-07 15:23:21 +0200

    second edition

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const  App =() => {
     <Button text={'btn 1'} backgroundColor={'black'} size="100px" color="colorViolet"/>
     <Button text={'btn 2'} backgroundColor={'green'} size="150px" color="colorWhite"/>
     </div>
-    <div className="general">
+    <div className="general input">
     <Input placeholder="first" backgroundColor="red" />
     <Input placeholder="second" backgroundColor="black"/>
     <Input placeholder="third" backgroundColor="green"/>
--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,5 +1,5 @@
 // react
-import { FC } from "react"
+import React, { FC, ChangeEvent } from "react";
 // libs
 import classnames from 'classnames'
 // styles
@@ -7,13 +7,20 @@ import styles from './Input.module.css'
 
 interface InputProps {
     placeholder : string;
-    onChange ?: () => void;
+    onChange ?: (event: ChangeEvent<HTMLInputElement>) => void;
     backgroundColor : 'red' | 'black' | 'green';
 }
 
 export const Input : FC<InputProps> = ({onChange, placeholder, backgroundColor}) => {
+
+    const funcInputValue = (event: ChangeEvent<HTMLInputElement>) => {
+        const valueOfInput = event.target.value;
+        console.log(valueOfInput);
+    };
+    
+
     return (
-        <input type="text" placeholder={placeholder} className={classnames(
+        <input id="myInput" type="text" placeholder={placeholder} onChange={funcInputValue} className={classnames(
             styles.input,{
                 [styles.IP1] : backgroundColor === 'black',
                 [styles.IP2] : backgroundColor === 'green',
